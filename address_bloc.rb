58ecd377504be4fa89786b6475c229d1507d1370
webdevbrent <webdeveloperbrent@gmail.com>
2018-03-17 08:11:41 -0400

    Added menu controller functionality

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,9 @@
-puts "Welcome to AddressBloc!"
+require_relative 'controllers/menu_controller'
+
+menu = MenuController.new
+
+system "clear"
+
+puts "Welcome to AddressBloc!\n======================="
+
+menu.main_menu
